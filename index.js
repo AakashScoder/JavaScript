42dad473e0dc1d9c75d95c2dfa0d28b58431efaa
aakash <aakashjaiswal90261@gmail.com>
2025-01-01 10:44:43 +0530

    variable

---
+++ index.js
@@ -0,0 +1,34 @@
+var a = 20
+console.log(a);
+
+var a = 30
+
+console.log(a);
+
+a= 40
+console.log(a);
+
+
+/////     let
+
+
+let b = 20;
+console.log(b);
+
+
+ b = 30;
+ 
+console.log(b);
+
+
+/// variable name
+
+
+let marks = 85
+console.log(marks);
+
+let num ;
+console.log(num);
+
+let rat = false;
+console.log(typeof(rat));
