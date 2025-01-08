b6b2153aee7dc4871d7d872f828269b5654aa414
aakash <aakashjaiswal90261@gmail.com>
2025-01-08 07:45:40 +0530

    practice

---
+++ Lecture2/index2.js
@@ -0,0 +1,39 @@
+console.log("heelo"
+
+);
+
+///   Arithmetic  operator
+
+let a = 10;
+let b = 10;
+console.log(a+b);
+
+
+let age = 25;
+let status1 = (age>18) ? 'i can vote' : 'i cannot Vote';
+console.log(status1);
+
+
+
+
+//  /let car = (true && true && false)
+//console.log(car);
+
+
+//let car = (false && true && true)
+//console.log(car);
+
+//let car = (false || true || true);
+//
+
+console.log(false || 7 || 5 || 22);
+
+// bitwise and or 
+
+console.log(2|5);
+console.log(2&5);
+
+
+console.log(~0);
+
+
---
+++ lectue3/index.js
@@ -0,0 +1,12 @@
+
+// function defination 
+function sayMyName(){
+    console.log("my name is Aakash");
+    
+}
+
+
+// function use - for function call
+sayMyName()
+
+
